ab8b9b2feacce596684c1fd099c32a48e06b77a7
getaflaw <kocyak579@gmail.com>
2021-02-07 15:20:43 +0300

    burger fix

--- javascript/burger-menu.js
+++ javascript/burger-menu.js
@@ -2,9 +2,11 @@ const burger = document.querySelector("#humburger-menu");
 const checkboxMenu = document.querySelector('#menu__toogle');
 const ulItem = document.querySelector(".menu__box");
 const closeBtn = document.createElement('button');
+const bodyHidden = document.body;
 
 checkboxMenu.addEventListener('click', function() {
     ulItem.style.display = 'flex';
+    bodyHidden.style.overflow='hidden';
     closeBtn.classList.add('menu__close');
     closeBtn.addEventListener('click', function() {
         ulItem.style.display = 'none';
