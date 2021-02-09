0b0167ef39843e05a4a2c247824965bf58dcba57
getaflaw <kocyak579@gmail.com>
2021-02-09 21:19:25 +0300

    team adapt

--- javascript/team-discription.js
+++ javascript/team-discription.js
@@ -3,19 +3,37 @@ const coWorker = document.querySelector('.team-list');
 coWorker.addEventListener('click',function(event){
     if (!event.target.matches('button')) return
     var element = event.target;
-    var elementPerents = element.parentElement.nextElementSibling;
+    var elementPerents = element.parentElement.parentElement.lastElementChild;
     var currentHeight = elementPerents.clientHeight;
-    console.log(currentHeight);
-    if (currentHeight=="80"){
+    var imgCoworker = element.parentElement.previousElementSibling;
+    var bodyWidth = document.body.clientWidth;
+    if (currentHeight >= "80"){
         event.target.style.transform="none";
         elementPerents.style.height="0";
     }
     else {
         elementPerents.style.height="80px";
+        
         event.target.style.transform="rotate(180deg)";
+        console.log(currentHeight);
     }
-    let leng = event.target.length;
-    console.log(leng);
+    var cloneImg = imgCoworker.cloneNode(true);
+    var elementImgclone = element.parentElement.nextElementSibling;
+    
+    if (bodyWidth < '768'){
+        if (elementImgclone.style.display=="block"){
+            elementImgclone.remove();
+        }
+        else{
+        cloneImg.style.display="block";
+        cloneImg.style.opacity="1";
+        element.parentElement.after(cloneImg);
+        }
+        
+        
+
+    }
+    
 })
 
 
