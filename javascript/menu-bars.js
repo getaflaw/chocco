7a9fb1448c276866ee27967bea0b738bd43ca460
getaflaw <kocyak579@gmail.com>
2021-02-17 19:02:35 +0300

    menu 1100 work

--- javascript/menu-bars.js
+++ javascript/menu-bars.js
@@ -4,20 +4,24 @@ const hiddenSlide = document.querySelectorAll('.menu-items__content');
 
 sliderMenu.forEach(function(item){
     item.addEventListener('click',function(){
+        
         hiddenSlide.forEach(function(slide){
             slide.classList.remove("menu-items__content_active");
         });
         let currentSlide = item.nextElementSibling;
+        if (currentSlide.clientWidth > 0) {
+            currentSlide.classList.remove("menu-items__content_active");
+            
+        }
+        else {
+        currentSlide.classList.add("menu-items__content_active");
+        closeBtn.classList.add('menu__close');
+        currentSlide.appendChild(closeBtn);
+        closeBtn.addEventListener('click', function(){
+        closeBtn.remove();
+        currentSlide.classList.remove("menu-items__content_active");
         
-        setTimeout(function(){
-            currentSlide.classList.add("menu-items__content_active");
-            closeBtn.classList.add('menu__close');
-            currentSlide.appendChild(closeBtn);
-            closeBtn.addEventListener('click', function(){
-                closeBtn.remove();
-                currentSlide.classList.remove("menu-items__content_active");
         });
-        
-        },1000);
+    }
     });
 });
